62b1e11de85aa5ca9976a58467ddf667352a706b
Floury-Fabio <floury-fabio@hotmail.fr>
2019-12-03 15:45:44 +0100

    migrate activity

--- db/migrate/20191203143340_create_activities.rb
+++ db/migrate/20191203143340_create_activities.rb
@@ -2,6 +2,15 @@ class CreateActivities < ActiveRecord::Migration[5.2]
   def change
     create_table :activities do |t|
 
+			t.string :name
+			t.string :adress
+			t.decimal :price
+			t.text :description
+			t.string :picture
+			
+			t.belongs_to :Cities, index: true
+			t.belongs_to :Activities_category, index: true
+
       t.timestamps
     end
   end
