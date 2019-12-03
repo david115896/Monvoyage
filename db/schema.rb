4276af050ea7d4a301bacc3f0f31504750571a57
Floury-Fabio <floury-fabio@hotmail.fr>
2019-12-03 18:38:03 +0100

    migration after merge

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,33 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_03_140005) do
+ActiveRecord::Schema.define(version: 2019_12_03_165131) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "activities", force: :cascade do |t|
+    t.string "name"
+    t.string "adress"
+    t.decimal "price"
+    t.text "description"
+    t.string "picture"
+    t.bigint "Cities_id"
+    t.bigint "Activities_category_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["Activities_category_id"], name: "index_activities_on_Activities_category_id"
+    t.index ["Cities_id"], name: "index_activities_on_Cities_id"
+  end
+
+  create_table "carts", force: :cascade do |t|
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.bigint "activity_id"
+    t.index ["activity_id"], name: "index_carts_on_activity_id"
+    t.index ["user_id"], name: "index_carts_on_user_id"
+  end
 
   create_table "countries", force: :cascade do |t|
     t.string "name"
@@ -22,6 +45,21 @@ ActiveRecord::Schema.define(version: 2019_12_03_140005) do
     t.string "currency"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "first_name"
+    t.string "last_name"
+    t.text "address"
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
 end
