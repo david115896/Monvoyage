337fe95347c9fe65fd9891bfd2b7b6aada91cf0c
laurent martinez <martilaurier@gmail.com>
2019-12-04 16:17:00 +0100

    [in progress] fix some bugs in tables

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,7 +1,7 @@
 class Order < ApplicationRecord
-    has_many :Tickets
+    has_many :tickets
     has_many :Orders
-    has_many :activities, through :tickets
-    has_many :Cities, though :Activities
-    belongs_to User
+    has_many :activities, through: :tickets
+    has_many :Cities, through: :activities
+    belongs_to :user
 end
