7e505af7f2a0036c4d47852d740f430658d0d8dc
laurent martinez <martilaurier@gmail.com>
2019-12-04 15:20:24 +0100

    [in progress] fix bug on table activities_category

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -1,7 +1,7 @@
 class Activity < ApplicationRecord
-	
 		has_many :carts
     has_many :users, through: :carts
     has_many :sold_items
     has_many :tickets
+    belongs_to :activities_category
 end
--- app/models/order.rb
+++ app/models/order.rb
@@ -1,2 +1,7 @@
 class Order < ApplicationRecord
+    has_many :Tickets
+    has_many :Orders
+    has_many :activities, through :tickets
+    has_many :Cities, though :Activities
+    belongs_to User
 end
