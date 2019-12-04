8a580156f7fcd6799ffb2f6c9dfc22d83a4ed260
laurent martinez <martilaurier@gmail.com>
2019-12-04 13:27:31 +0100

    [in progress] views for new and update activity

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -3,4 +3,5 @@ class Activity < ApplicationRecord
 		has_many :carts
     has_many :users, through: :carts
     has_many :sold_items
+    has_many :tickets
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_04_101911) do
+ActiveRecord::Schema.define(version: 2019_12_04_120706) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "activities", force: :cascade do |t|
     t.string "name"
-    t.string "adress"
+    t.string "address"
     t.decimal "price"
     t.text "description"
     t.string "picture"
