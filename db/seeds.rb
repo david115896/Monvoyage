3ae0bc3fd858ff540ab96baada2743fb6699c7fc
laurent martinez <martilaurier@gmail.com>
2019-12-05 10:44:15 +0100

    [test] test-front

--- db/seeds.rb
+++ db/seeds.rb
@@ -14,12 +14,12 @@ puts "Country has been created"
 
 
 20.times do 
-  City.create(name: country:Country.all.sample, address:Faker::Address.city ,climat:"test",description:"test",timezone:"test",traditions:"test")
+  City.create(name: faker:: Country.all.sample, address: Faker:: Address.city ,climat:"test",description:"test",timezone:"test",traditions:"test")
 end 
 puts "City has been created"
 
 20.times do
-    Activity.create(name:Faker ::name, address: , price:, description:, picture:, cities_id: ,activities_category: )
+    Activity.create(name: Faker:: name, address: faker:: address , price: faker:: price, description: 'test', picture: "test", cities_id: '1',activities_category: 'test' )
 end
 
     #Activity.create(name:)
