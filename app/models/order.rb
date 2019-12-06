c3a013aea6c124efa58e78756b03fe5d5c3a567e
Floury-Fabio <floury-fabio@hotmail.fr>
2019-12-06 04:54:55 +0100

    order model correction

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,8 +1,7 @@
 class Order < ApplicationRecord
-    has_many :tickets
-    has_many :Orders
     has_many :activities, through: :tickets
-    has_many :Cities, through: :activities
+    has_many :cities, through: :activities
     has_many :sold_tickets
+    has_many :tickets, through: :sold_tickets
     belongs_to :user
 end
