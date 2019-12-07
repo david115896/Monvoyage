23b79527ee864d251e08b5186c243d696357b83d
Floury-Fabio <floury-fabio@hotmail.fr>
2019-12-07 15:39:24 +0100

    organiser upgrade migration

--- db/migrate/20191205201644_create_organisers.rb
+++ db/migrate/20191205201644_create_organisers.rb
@@ -1,7 +1,14 @@
 class CreateOrganisers < ActiveRecord::Migration[5.2]
   def change
     create_table :organisers do |t|
+
+			t.boolean :selected
+			t.boolean :paid
+
       t.belongs_to :user, index: true
+      t.belongs_to :city, index: true
+      t.belongs_to :ticket, index: true
+
       t.timestamps
     end
   end
