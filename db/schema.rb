7f7662eb7abe1a0de798f467769953a8a718fe1b
Floury-Fabio <floury-fabio@hotmail.fr>
2019-12-10 19:30:37 +0100

    after migration of duration to organisers

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_10_182404) do
+ActiveRecord::Schema.define(version: 2019_12_10_182741) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -96,6 +96,7 @@ ActiveRecord::Schema.define(version: 2019_12_10_182404) do
     t.bigint "city_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "duration"
     t.index ["city_id"], name: "index_organisers_on_city_id"
     t.index ["user_id"], name: "index_organisers_on_user_id"
   end
