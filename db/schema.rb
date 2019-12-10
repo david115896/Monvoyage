caec143be6136845b608452d0db3291ebca69c4e
Floury-Fabio <floury-fabio@hotmail.fr>
2019-12-10 19:27:00 +0100

    after migration of days to checkouts

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_10_110149) do
+ActiveRecord::Schema.define(version: 2019_12_10_182404) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -54,6 +54,7 @@ ActiveRecord::Schema.define(version: 2019_12_10_110149) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "index"
+    t.integer "day"
     t.index ["organiser_id"], name: "index_checkouts_on_organiser_id"
     t.index ["ticket_id"], name: "index_checkouts_on_ticket_id"
   end
