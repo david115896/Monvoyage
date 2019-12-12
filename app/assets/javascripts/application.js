df80f6d1af88d92d06d58a8f45ab92cf640b6a23
david115896 <dtaccori@gmail.com>
2019-12-12 01:02:40 +0100

    [done] added notifications with toastr

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,6 +12,25 @@
 //= require rails-ujs
 //= require jquery
 //= require jquery_ujs
+//= require toastr
 //= require activestorage
 //= require turbolinks
 //= require_tree .
+
+// code for toastr
+$(document).ready(function() {  
+    toastr.options = {
+        "closeButton": false,
+        "debug": false,
+        "positionClass": "toast-top-right",
+        "onclick": null,
+        "showDuration": "300",
+        "hideDuration": "1000",
+        "timeOut": "5000",
+        "extendedTimeOut": "1000",
+        "showEasing": "swing",
+        "hideEasing": "linear",
+        "showMethod": "fadeIn",
+        "hideMethod": "fadeOut"
+    }
+});
