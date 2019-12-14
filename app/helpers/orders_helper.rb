6f69246448a9352e47881a40f7ed11c73e4918ca
david115896 <dtaccori@gmail.com>
2019-12-14 13:41:18 +0100

    [DONE] finished design of dashboard

--- app/helpers/orders_helper.rb
+++ app/helpers/orders_helper.rb
@@ -1,2 +1,10 @@
 module OrdersHelper
+
+    def amount_order(order)
+        amount = 0
+        order.tickets.each do |ticket|
+            amount += ticket.price
+        end
+        return amount
+	end
 end
